b2da6fb28f139da231b9ba6e3010793958d1b253
Abhishek Thory <abhithory@gmail.com>
2021-11-23 16:37:23 +0530

    Updates

---
+++ precache-manifest.1b002d83f6e71b7914280df51131d269.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "adf1c45253825659339b",
+    "url": "/lottery_game/static/css/main.afd56a0e.chunk.css"
+  },
+  {
+    "revision": "adf1c45253825659339b",
+    "url": "/lottery_game/static/js/main.adf1c452.chunk.js"
+  },
+  {
+    "revision": "ef57b6cdf9439e5542fa",
+    "url": "/lottery_game/static/css/1.ce284af8.chunk.css"
+  },
+  {
+    "revision": "ef57b6cdf9439e5542fa",
+    "url": "/lottery_game/static/js/1.ef57b6cd.chunk.js"
+  },
+  {
+    "revision": "280a084105d6e5f980fe",
+    "url": "/lottery_game/static/js/runtime~main.280a0841.js"
+  },
+  {
+    "revision": "7fb23f3b56a834d9dcf615b5e7c78da8",
+    "url": "/lottery_game/static/media/bootstrap-icons.7fb23f3b.woff"
+  },
+  {
+    "revision": "e07b538aa51b6fa77f32828af21cb591",
+    "url": "/lottery_game/static/media/bootstrap-icons.e07b538a.woff2"
+  },
+  {
+    "revision": "b37d7741d7060a22444858f6af5c7dd8",
+    "url": "/lottery_game/static/media/banner2.b37d7741.jpg"
+  },
+  {
+    "revision": "2d3d018daaa43e7ccca4c8e96d95e11d",
+    "url": "/lottery_game/index.html"
+  }
+];
